c7defb508527ada3b3de01a40b8d672025e73e74
Conwell Maraya <conwell.maraya@gmail.com>
2025-08-06 18:06:18 +0800

    Hope it works this time around
    Take 2

--- Linux/packageinstaller.sh
+++ Linux/packageinstaller.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-
-pkgs=(bless-unofficial wireshark)
-sudo apt-get -y --ignore-missing install "${pkgs[@]}" 
+version https://git-lfs.github.com/spec/v1
+oid sha256:97b93b0943cafe33ded99e7134aae3244d42325538ca901bdce1db9dd4d8777e
+size 104
