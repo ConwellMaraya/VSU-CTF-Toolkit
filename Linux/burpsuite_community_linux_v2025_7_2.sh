65dd1252bdaece48fc681cf45fafcb8fa8cef47c
Conwell Maraya <conwell.maraya@gmail.com>
2025-08-08 03:56:59 +0800

    Fucked up the burp installers
    Turns out i downloaded the professional edition and not the community one, whoops.
    Fixed that

---
+++ Linux/burpsuite_community_linux_v2025_7_2.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:918edc1c57fed3542f6c2bdce0aa812c267afc7593e744b87267b8e87ea18604
+size 338713664
--- Linux/burpsuite_pro_linux_v2025_6_5.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d705405431810bcb04557f6c04ca636b48ac4e31f9852c7b2daa31be5d81f452
-size 337757988
